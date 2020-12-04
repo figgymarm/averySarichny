3dbb3db5bf6b0244abc75e0dfdadcffa68b44c8e
Avery Sarichny <averysarichny@cpe-67-240-191-189.rochester.res.rr.com>
2020-12-03 19:59:17 -0500

    fixed user input, added css and changed the layout of the data on the page

--- app.js
+++ app.js
@@ -23,18 +23,18 @@ $(() => {
   $('form').on('submit', (event) => {
       event.preventDefault();
 
-  const userInput = $('input[type="text"]').val();
+  const $userInput = $('input[type="text"]').val();
+
   $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/weather?q=London&appid=cbd693e99b02f7879da60ac109802e62",
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${$userInput}&appid=cbd693e99b02f7879da60ac109802e62`,
         type: "GET",
         dataType: "JSONp"
       }).then(
         (data) => {
           //handler for success
-          // console.log(data);
-          // console.log(data.weather);
           $('#weather').html(data.weather[0].description);
-          $('#main').html(data.weather[0].main);
+          console.log(data);
+          $('#main').html(data.main.temp);
           $('#icon').html(data.weather[0].icon);
         },
         (error) => {
