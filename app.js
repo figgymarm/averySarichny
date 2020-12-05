75466718925f40617c1fb973db5abca86f586615
Avery Sarichny <averysarichny@cpe-67-240-191-189.rochester.res.rr.com>
2020-12-05 09:09:59 -0500

    added raindrop and some div css

--- app.js
+++ app.js
@@ -5,26 +5,37 @@
 //*************************
 //ON CLICK API FUNCTION
 //*************************
-
 $(() => {
   $("form").on("submit", (event) => {
     event.preventDefault();
 
     const $userInput = $('input[type="text"]').val();
-
     $.ajax({
       url: `https://api.openweathermap.org/data/2.5/weather?q=${$userInput}&appid=cbd693e99b02f7879da60ac109802e62`,
       type: "GET",
       dataType: "JSONp",
     }).then(
       (data) => {
-        //handler for success
-        const $weather = data.weather[0].description;
-        $("#weather").html(data.weather[0].description);
-        $("#canvas").html(data.name);
-        if(weather.indexOf("rain") >= 0) {
-          skycons.set("animated-icon", Skycons.RAIN);
-        }
+        const $cityInfo = $("#description").html(data.weather[0].description);
+        //condition for 50/50
+        if (
+          $cityInfo.text() === "cloudy" ||
+          "broken clouds" ||
+          "scattered clouds" ||
+          "overcast" ||
+          "mist" ||
+          "clouds"
+        ) {
+            const $raindrop = $("<img>", {
+              src: "imgs/raindrop.png",
+            });
+            $raindrop.attr("width", "70px").attr("height", "100px");
+            $raindrop.appendTo("#rainAnswer");
+            alert(
+              `It's looking like ${data.weather[0].description} in ${$userInput} today.`
+            );
+          };
+        //insert a method to remove city from text input
       },
       (error) => {
         //handler for errors
